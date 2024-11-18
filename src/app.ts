f414f77764064f3ee5f5da6019ea42286139695a
Oluwatobi97 <victortobi2000@gamil.com>
2024-11-18 14:43:52 +0100

    app changes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Express } from 'express'
-// import cors from 'cors'
-// import errorHandler from './utils/errorhandle'
+import cors from 'cors'
+import errorHandler from './utils/errorhandle'
 // import { userRouter } from './resources/user-resource/routes/user-routes'
 // import { investMentRouter } from './resources/investment-plan-resources/investment-routes'
-// import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 
 export const app = express()
 
@@ -11,15 +11,15 @@ export const app = express()
 
 
 
-// app.use(cookieParser())
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
-// app.use(express.json())
-// // app.use('/api/v1/auth-system', userRouter)
-// // app.use('/api/v1/investment', investMentRouter)
-// app.use(errorHandler)
+app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+app.use(express.json())
+// app.use('/api/v1/auth-system', userRouter)
+// app.use('/api/v1/investment', investMentRouter)
+app.use(errorHandler)
 
 // export class App {
 //     app: Express
