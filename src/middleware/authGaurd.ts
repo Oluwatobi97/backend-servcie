5a35931ed3b459f2b077fbac3350ae30f842dab4
Oluwatobi97 <victortobi2000@gamil.com>
2024-11-21 13:18:21 +0100

    req-quries

--- src/middleware/authGaurd.ts
+++ src/middleware/authGaurd.ts
@@ -26,8 +26,8 @@ const convertToJwtPayload = (token:string)=>{
 export const authGaurd = (req: Request, res: Response, next: NextFunction) => {
     const cookieToken = req.cookies?.accessToken
     const authorizationToken = JSON.stringify(req.query.token)
-    const token =authorizationToken.replace(/^"|"$/g, "");
-    console.log(token, 'tokens')
+    // const token =authorizationToken.replace(/^"|"$/g, "");
+    console.log(authorizationToken, 'tokens')
     if (!cookieToken) throw new UnAuthorized('un-Authorized')
     
         const jwtPayload = decrypt(cookieToken)
