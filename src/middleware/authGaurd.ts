5008b1fffe68b19fa1ba7767471b192ac8295569
Oluwatobi97 <victortobi2000@gamil.com>
2024-11-21 11:32:46 +0100

    adding query

--- src/middleware/authGaurd.ts
+++ src/middleware/authGaurd.ts
@@ -25,7 +25,7 @@ const convertToJwtPayload = (token:string)=>{
 
 export const authGaurd = (req: Request, res: Response, next: NextFunction) => {
     const cookieToken = req.cookies?.accessToken
-    const authorizationToken = req.headers
+    const authorizationToken = req.query
     console.log(authorizationToken, 'token')
     if (!cookieToken) throw new UnAuthorized('un-Authorized')
     
