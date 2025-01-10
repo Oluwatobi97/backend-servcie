9c6a5ab4df0a4da6f8a0e04d69ba77f90cc678f7
Ktbch <odufotekayode4@gmail.com>
2025-01-10 12:19:45 +0100

    database-url connection change

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     schema: './src/schemas/index.ts',
     out: './drizzle',
     dbCredentials: {
-        url: 'postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a.oregon-postgres.render.com/investment_app_2lk2'
+        url: 'postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a/investment_app_2lk2'
     }
 
 })
--- src/db/db-connect.ts
+++ src/db/db-connect.ts
@@ -3,6 +3,6 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres';
 
 
-const queryClient = postgres("postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a.oregon-postgres.render.com/investment_app_2lk2");
+const queryClient = postgres("postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a/investment_app_2lk2");
 // postgresql://tobi:NGgTeFxG3WiHmLAh44yVMPYPYq502MzY@dpg-cstkfhogph6c739frsng-a/invetment
 export const db = drizzle(queryClient);
