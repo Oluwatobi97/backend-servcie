a4dde7f32722f5460b3082326e6a50c63e323098
Ktbch <odufotekayode4@gmail.com>
2025-01-12 22:41:33 +0100

    DATABSE_URL_CHANGE_URL CHANGE

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     schema: './src/schemas/index.ts',
     out: './drizzle',
     dbCredentials: {
-        url: 'postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a/investment_app_2lk2'
+        url: 'postgresql://kayode:xbQqndwd9nO1R8T9skcBAP5JTxyHYHog@dpg-cu23b71u0jms738nfnt0-a/investment_app_xxjq'
     }
 
 })
