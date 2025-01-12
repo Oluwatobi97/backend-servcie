e904dd824d470b27965c5133455ba212afd00841
Ktbch <odufotekayode4@gmail.com>
2025-01-12 22:27:28 +0100

    BASE_URL CHANGE

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     schema: './src/schemas/index.ts',
     out: './drizzle',
     dbCredentials: {
-        url: 'postgresql://investment_5529_user:FZk0vtlfgGd6aJb5uhLEwqaH7LnTLPD1@dpg-cu0gna1u0jms73d18lfg-a/investment_5529'
+        url: 'postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a/investment_app_2lk2'
     }
 
 })
--- src/db/db-connect.ts
+++ src/db/db-connect.ts
@@ -3,6 +3,6 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres';
 
 
-const queryClient = postgres("postgresql://investment_5529_user:FZk0vtlfgGd6aJb5uhLEwqaH7LnTLPD1@dpg-cu0gna1u0jms73d18lfg-a/investment_5529");
+const queryClient = postgres("postgresql://kayode:mUTCPeinys3Bd5CNpiB1WzcqG9nqQgYw@dpg-cu0fq65ds78s73dd7di0-a/investment_app_2lk2");
 // postgresql://tobi:NGgTeFxG3WiHmLAh44yVMPYPYq502MzY@dpg-cstkfhogph6c739frsng-a/invetment
 export const db = drizzle(queryClient);
