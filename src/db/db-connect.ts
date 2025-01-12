464479f1da230bcad8822b047005a0cb006ffda0
Oluwatobi97 <54982980+Oluwatobi97@users.noreply.github.com>
2025-01-13 00:12:19 +0100

    Update db-connect.ts
    backend fix

--- src/db/db-connect.ts
+++ src/db/db-connect.ts
@@ -3,6 +3,6 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres';
 
 
-const queryClient = postgres("postgresql://investment_5529_user:FZk0vtlfgGd6aJb5uhLEwqaH7LnTLPD1@dpg-cu0gna1u0jms73d18lfg-a/investment_5529");
+const queryClient = postgres("postgresql://investment_bftn_user:eL2JbfqmS2SxxG5VuJeREgf4uxVU8LR9@dpg-cu24mj1opnds73ak9n00-a/investment_bftn");
 // postgresql://tobi:NGgTeFxG3WiHmLAh44yVMPYPYq502MzY@dpg-cstkfhogph6c739frsng-a/invetment
 export const db = drizzle(queryClient);
