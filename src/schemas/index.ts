13494ecbc5287f664d1631a527039ac3f6a6028d
Oluwatobi97 <54982980+Oluwatobi97@users.noreply.github.com>
2025-01-13 09:34:43 +0100

    Update index.ts
    Update index.ts

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,8 @@
 import { pgTable, unique, serial, integer, timestamp, text, pgEnum } from "drizzle-orm/pg-core"
 
-// export const coins = pgEnum("coins", [ 'BTC', 'ETH', 'USDT' ])
-//export const duration = pgEnum("duration", [ '1-day', '1-month', '3-month', '6-month', '1-year' ])
-//export const investment = pgEnum("investment", [ 'Gold', 'Silver', 'Bronze' ])
+export const coins = pgEnum("coins", [ 'BTC', 'ETH', 'USDT' ])
+export const duration = pgEnum("duration", [ '1-day', '1-month', '3-month', '6-month', '1-year' ])
+export const investment = pgEnum("investment", [ 'Gold', 'Silver', 'Bronze' ])
 
 
 
@@ -17,9 +17,9 @@ export const usersTable = pgTable("users", {
 
 export const investnmentPlanB = pgTable("investnment-planB", {
     id: serial().primaryKey().notNull(),
-    //planType: investment().notNull(),
-    //coinsType: coins().notNull(),
-    //investMentDuration: duration().notNull(),
+    planType: investment().notNull(),
+    coinsType: coins().notNull(),
+    investMentDuration: duration().notNull(),
     amount: integer().notNull(),
     createdAt: timestamp("created-at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
     updatedAt: timestamp("updated-at", { withTimezone: true, mode: 'string' }).defaultNow().notNull(),
